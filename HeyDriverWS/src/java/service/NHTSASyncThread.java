d6b517134a2207f71c6ca59c356478d30de114e1
luiisperez <luis.alejandro120895@gmail.com>
2017-10-27 16:16:10 -0400

    Base de datos de marcas y modelos

--- HeyDriverWS/src/java/service/NHTSASyncThread.java
+++ HeyDriverWS/src/java/service/NHTSASyncThread.java
@@ -5,6 +5,11 @@
  */
 package service;
 
+import controller.cars_module.SyncNHTSADataCommand;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.logging.Level;
@@ -23,10 +28,22 @@ public class NHTSASyncThread implements Runnable{
         while (execute_thread){
             try {
                 Thread.sleep(30000); //CANTIDAD DE TIEMPO ENTRE CADA SINCRONIZACION CON EL SERVIDOR
-                System.out.println(i);
-                i = i + 30;
-            } catch (InterruptedException ex) {
-                Logger.getLogger(NHTSASyncThread.class.getName()).log(Level.SEVERE, null, ex);
+                SyncNHTSADataCommand sync = new SyncNHTSADataCommand();
+                sync.execute();
+                DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+
+                LocalDateTime now = LocalDateTime.now();
+                System.out.println(dtf.format(now));
+                System.out.println("SINCRONIZACION COMPLETADA. FECHA Y HORA DE CULMINACION: " + dtf.format(now));
+            } catch (Exception ex) {
+
+                DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+
+                LocalDateTime now = LocalDateTime.now();
+                System.out.println(dtf.format(now));
+                System.out.println("SINCRONIZACION FALLIDA. FECHA Y HORA DE CULMINACION: " + dtf.format(now));
             }
         }
         try {
