b6af06148831b6885e4b6016b1c13ab1c1dd0fc9
Cristian Dos Ramos <crisdosramos@gmail.com>
2017-11-10 02:23:49 -0400

    Controlador del perceptrón multicapa en Neuroph
    Clase para la carga, entrenamiento y prueba del perceptrón que realizará
    las salidas.
    - Falta configurar las entradas y salidas del entrenamiento.

--- HeyDriverWS/src/java/common/entities/DiagnosticMultilayerPerceptron.java
+++ HeyDriverWS/src/java/common/entities/DiagnosticMultilayerPerceptron.java
@@ -16,5 +16,55 @@ import org.neuroph.util.TransferFunctionType;
  * @author cristian
  */
 public class DiagnosticMultilayerPerceptron {
+    private int entradas;
+    private int salidas;
+    private MultiLayerPerceptron myMlPerceptron;
+
     
+    public static void bildRNA() {
+
+        // create training set (logical Diagnostic function)
+        DataSet trainingSet = new DataSet(16, 10);
+        
+        
+        // ENTRENAMIENTO
+        // aquí entrarían 16 valores y saldrían 10 valores
+//        trainingSet.addRow(new DataSetRow(new double[]{0, 0}, new double[]{0}));
+//        trainingSet.addRow(new DataSetRow(new double[]{0, 1}, new double[]{1}));
+//        trainingSet.addRow(new DataSetRow(new double[]{1, 0}, new double[]{1}));
+//        trainingSet.addRow(new DataSetRow(new double[]{1, 1}, new double[]{0}));
+
+        // create multi layer perceptron: in:16, shadow: 12, out: 10
+        MultiLayerPerceptron myMlPerceptron = new MultiLayerPerceptron(TransferFunctionType.TANH, 16, 12, 10);
+        // learn the training set
+        myMlPerceptron.learn(trainingSet);
+
+        // prueba perceptron
+        System.out.println("Testing trained neural network");
+        testNeuralNetwork(myMlPerceptron, trainingSet);
+
+        // save trained neural network
+        myMlPerceptron.save("myMlPerceptron.nnet");
+
+        // load saved neural network
+        NeuralNetwork loadedMlPerceptron = NeuralNetwork.createFromFile("myMlPerceptron.nnet");
+
+        // Probar la red
+        System.out.println("Testing loaded neural network");
+        testNeuralNetwork(loadedMlPerceptron, trainingSet);
+
+    }
+    
+    // función para probar salidas según entradas.
+    public static void testNeuralNetwork(NeuralNetwork nnet, DataSet testSet) {
+
+        for(DataSetRow dataRow : testSet.getRows()) {
+            nnet.setInput(dataRow.getInput());
+            nnet.calculate();
+            double[ ] networkOutput = nnet.getOutput();
+            System.out.print("Input: " + Arrays.toString(dataRow.getInput()) );
+            System.out.println(" Output: " + Arrays.toString(networkOutput) );
+        }
+
+    }
 }
