10b6cdbc14f7d2de602942032e95984ca5ff54ca
Cristian Dos Ramos <crisdosramos@gmail.com>
2017-11-10 00:47:20 -0400

    Librería Neuroph y DiagnosticMultilayerPerceptron
    + Motor de neuroph
    + Clase controladora de la red neuronal

---
+++ HeyDriverWS/src/java/common/entities/DiagnosticMultilayerPerceptron.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package common.entities;
+import java.util.Arrays;
+import org.neuroph.core.NeuralNetwork;
+import org.neuroph.nnet.MultiLayerPerceptron;
+import org.neuroph.core.data.DataSet;
+import org.neuroph.core.data.DataSetRow;
+import org.neuroph.util.TransferFunctionType;
+
+/**
+ *
+ * @author cristian
+ */
+public class DiagnosticMultilayerPerceptron {
+    
+}
