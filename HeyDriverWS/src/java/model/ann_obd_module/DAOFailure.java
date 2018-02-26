2f9d511e238bbc57dfd7c6665d6c80a1a67874f4
luiisperez <luis.alejandro120895@gmail.com>
2018-02-25 21:11:29 -0400

    Arreglo en llamada a SP

--- HeyDriverWS/src/java/model/ann_obd_module/DAOFailure.java
+++ HeyDriverWS/src/java/model/ann_obd_module/DAOFailure.java
@@ -23,7 +23,7 @@ import org.eclipse.jdt.internal.compiler.ast.ForeachStatement;
  */
 public class DAOFailure  extends DAO{
     private Connection _bdCon;
-    private static String _sqlAddFailure = "{?=call ADD_FAILURE(?,?,?)}";
+    private static String _sqlAddFailure = "{?=call ADD_FAILURE(?,?,?,?)}";
     private static String _sqlGetAllFailures = "{call GET_ALL_FAILURES()}";
     private static String _sqlGetAllFailuresByBrand = "{call GET_FAILURES_BY_BRAND(?)}";
     private static String _sqlGetAllFailuresByModel = "{call GET_FAILURES_BY_MODEL(?,?)}";
